ae841f0d0b9b79bf5c5a23c9ef1e727e27530c51
diwpro007 <diwserala@gmail.com>
2020-07-27 08:36:41 +0545

    Git added the basic components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    <div>
-      <h1>App</h1>
-    </div>;
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
   }
 }
 
