e08891a34fdffab32bc0bb017094e84f519f3933
diwpro007 <diwserala@gmail.com>
2020-07-29 22:12:20 +0545

    Updates

---
+++ precache-manifest.83ba18036318baa223f6b0f51dadd0cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68193453a632cf1f1ae60455ccaf411a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "c3a9a9b7e64edd08d4b9",
+    "url": "/covid-19-tracker/static/css/main.4f6f0524.chunk.css"
+  },
+  {
+    "revision": "bc1c98d14aad81fd8384",
+    "url": "/covid-19-tracker/static/js/2.9350cec7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.9350cec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a9a9b7e64edd08d4b9",
+    "url": "/covid-19-tracker/static/js/main.f81b4b85.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
